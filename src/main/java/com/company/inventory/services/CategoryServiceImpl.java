d351a334f42df6bafb35926cfab70baae18f42d1
ariandwe <arian5770657@gmail.com>
2024-11-22 23:10:37 -0500

    servicio GET categorias

---
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -0,0 +1,49 @@
+package com.company.inventory.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.company.inventory.dao.ICategoryDao;
+import com.company.inventory.model.Category;
+import com.company.inventory.response.CategoryResponseRest;
+
+@Service
+public class CategoryServiceImpl  implements ICategoryService{
+	
+	@Autowired
+	private ICategoryDao categoryDao;
+	
+	
+    @Override
+    @Transactional(readOnly= true)
+    public ResponseEntity<CategoryResponseRest> search() {
+	
+    	CategoryResponseRest response = new CategoryResponseRest();
+    	
+    	try{
+    		List<Category> category = (List<Category>) categoryDao.findAll();
+    				
+    		response.getCateogryResponse().setCategory(category);
+    		response.setMetadata("Respuesta Ok", "00", "Respuesta exitosa");
+    				
+    	} catch (Exception e) {
+
+    		response.setMetadata("Fail", "-1", "Respuesta error");
+    		e.getStackTrace();
+
+        	return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    		
+    	}
+    	
+    	return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+    	
+}
+    
+    
+    
+}
